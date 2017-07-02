f2675ac9457ec3115fd9b8fcacc9100c2342baaa
idSaid <id.said@live.fr>
2017-07-02 18:10:59 +0100

    Tbs Model

---
+++ src/model/Dbf.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Dbf {
+
+	private String name;
+	private String path;
+	private Long size;
+	private TableSpace tableSpace;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public Long getSize() {
+		return size;
+	}
+	public void setSize(Long size) {
+		this.size = size;
+	}
+	public TableSpace getTableSpace() {
+		return tableSpace;
+	}
+	public void setTableSpace(TableSpace tableSpace) {
+		this.tableSpace = tableSpace;
+	}
+	public Dbf() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Dbf(String name, String path, Long size, TableSpace tableSpace) {
+		super();
+		this.name = name;
+		this.path = path;
+		this.size = size;
+		this.tableSpace = tableSpace;
+	}
+	
+	
+	
+}
---
+++ src/model/TableSpace.java
@@ -0,0 +1,56 @@
+package model;
+
+import utils.TableSpaceType;
+
+public class TableSpace {
+	
+	private String name;
+	private Long size;
+	private String status;
+	private TableSpaceType type;
+	private double fillingRate;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getSize() {
+		return size;
+	}
+	public void setSize(Long size) {
+		this.size = size;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public TableSpaceType getType() {
+		return type;
+	}
+	public void setType(TableSpaceType type) {
+		this.type = type;
+	}
+	public double getFillingRate() {
+		return fillingRate;
+	}
+	public void setFillingRate(double fillingRate) {
+		this.fillingRate = fillingRate;
+	}
+	public TableSpace() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public TableSpace(String name, Long size, String status, TableSpaceType type, double fillingRate) {
+		super();
+		this.name = name;
+		this.size = size;
+		this.status = status;
+		this.type = type;
+		this.fillingRate = fillingRate;
+	}
+	
+	
+}
