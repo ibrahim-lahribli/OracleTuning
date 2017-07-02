e2558c6c274c5efed8d07d8fc67469e850f67994
Dizay93 <elbadri.yazid@gmail.com>
2017-07-02 21:08:07 +0100

    dao:DbConfig

---
+++ src/dao/DbConfig.java
@@ -0,0 +1,67 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbConfig {
+	public static Connection con; // objet de type connection
+	public static ResultSet rs = null; // pour le jeu de resultat
+	public static PreparedStatement pst; // pour les requetes parametes
+	public static java.sql.Statement st; // pour les requetes simples
+	public static String instance ;
+	
+	
+
+	public static String getInstance() {
+		return instance;
+	}
+
+	public static void setInstance(String instance) {
+		DbConfig.instance = instance;
+	}
+
+	public static void Connect() throws SQLException {
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); 
+			String url = "jdbc:oracle:thin:@localhost:1521:"+instance; 
+			con = (Connection) DriverManager.getConnection(url, "sysdba", "sysdba");
+			System.out.println("Connection réussie");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void disconnect() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static ResultSet select(String sql) {
+		try {
+			rs = (ResultSet) st.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public static int update(String sql) {
+		int nb = 0;
+		try {
+			nb = st.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return nb;
+	}
+
+}
