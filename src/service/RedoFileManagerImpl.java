d27d149759bfeef8d870c59accfb31f91decda4f
Dizay93 <elbadri.yazid@gmail.com>
2017-07-23 13:52:20 +0100

    Implementation des methodes du RedofileManager dans le RedoFileManagerImpl + Implementation des methodes du LogBFManager dans le LogBFManagerImpl

--- src/service/RedoFileManagerImpl.java
+++ src/service/RedoFileManagerImpl.java
@@ -1,5 +1,12 @@
 package service;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+import dao.DbConfig;
 import model.RedoFile;
 
 public class RedoFileManagerImpl implements RedoFileManager{
@@ -7,18 +14,53 @@ public class RedoFileManagerImpl implements RedoFileManager{
 	@Override
 	public void addFile(RedoFile redoFile) {
 		// TODO Auto-generated method stub
+		 try {
+	            
+	            DbConfig.Connect();
+	            String sql="alter database add LOGFILE('"+redoFile.getPath()+"') '"+redoFile.getSize()+"'";
+	            DbConfig.update(sql);
+	            DbConfig.disconnect();
+	            
+	           
+	            
+	        } catch (SQLException ex) {
+	            Logger.getLogger(RedoFile.class.getName()).log(Level.SEVERE, null, ex);
+	        }
 		
 	}
 
 	@Override
 	public void removeFile(RedoFile redoFile) {
 		// TODO Auto-generated method stub
+		 try {
+	            
+	            DbConfig.Connect();
+	            String sql="alter database drop LOGFILE MEMBER '"+redoFile.getPath()+"'";
+	            DbConfig.update(sql);
+	            DbConfig.disconnect();
+	            
+	           
+	            
+	        } catch (SQLException ex) {
+	            Logger.getLogger(RedoFile.class.getName()).log(Level.SEVERE, null, ex);
+	        }
 		
 	}
 
 	@Override
 	public void setSize(RedoFile redoFile, int size) {
 		// TODO Auto-generated method stub
+		try{
+			DbConfig.Connect();
+	        String sql="alter database modify LOGFILE MEMBER '"+redoFile.getPath()+"' '"+redoFile.getSize()+"'";
+	        DbConfig.update(sql);
+	        DbConfig.disconnect();
+			
+			
+		 } catch (SQLException ex) {
+	         JOptionPane.showMessageDialog(null, ex);
+	        
+	    }
 		
 	}
 
