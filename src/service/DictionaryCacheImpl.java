104bb18668703f19eac1122a01fafbea8a02acb3
saad330 <saad.hamama330@gmail.com>
2017-07-24 14:49:25 +0200

    DAO

--- src/service/DictionaryCacheImpl.java
+++ src/service/DictionaryCacheImpl.java
@@ -1,48 +1,25 @@
 package service;
 
-import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
-
-import dao.DbConfig;
+import dao.DictionaryCacheDAO;
 import model.DictionaryCache;
 
-public class DictionaryCacheImpl implements DictionaryCacheManager {
+public class DictionaryCacheImpl {
+DictionaryCacheDAO dcd = new DictionaryCacheDAO();
 
-	@Override
 	public DictionaryCache getR() {
 		// TODO Auto-generated method stub
 		DictionaryCache dc=null;
-		  try {
-	            
-	            DbConfig.Connect();
-	            String sql="select sum(gets) , sum(getmisses)  , sum(getmisses)/(sum(gets)+sum(getmisses))*100 as R from v$rowcache ";
-	            DbConfig.pst= DbConfig.con.prepareStatement(sql);
-	            DbConfig.rs= DbConfig.pst.executeQuery(sql);
-
-	        } catch (SQLException ex) {
-	             JOptionPane.showMessageDialog(null, ex);
-	            
-	        }
+		 dcd.getR();
 	      
 		 return dc;
 	}
 
-	@Override
+
 	public void setSize(int size) {
 		// TODO Auto-generated method stub
-		try{
-			DbConfig.Connect();
-	        String sql="ALTER SYSTEM SET SHARED_POOL_SIZE ='"+size+"'";
-	        DbConfig.update(sql);
-	        DbConfig.disconnect();
-			
-			
-		 } catch (SQLException ex) {
-	         JOptionPane.showMessageDialog(null, ex);
-	        
-	    }
 		
+		dcd.setSize(size);
 	}
 
 }
