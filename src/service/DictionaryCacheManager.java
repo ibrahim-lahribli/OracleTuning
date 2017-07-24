776fadeb28d335664a9bd03b55fe4e99edb91a12
saad330 <saad.hamama330@gmail.com>
2017-07-24 13:56:07 +0200

    dictionaryCache+LibraryCache Models and Managers

---
+++ src/service/DictionaryCacheManager.java
@@ -0,0 +1,10 @@
+package service;
+
+import model.DictionaryCache;
+
+public interface DictionaryCacheManager {
+	public DictionaryCache getR();
+	public void setSize(int size);
+
+
+}
---
+++ src/service/LibraryCacheManager.java
@@ -0,0 +1,9 @@
+package service;
+
+import model.LibraryCache;
+
+public interface LibraryCacheManager {
+	public LibraryCache getR() ;
+	public void setSize(int size);
+
+}
